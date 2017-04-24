8343526375bb601436e9424e40d99efed4261303
Charlie Faber <charliefaber@gmail.com>
2017-04-24 01:03:35 -0400

    test

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.set('view engine', 'handlebars');
 
 //routers
 //require('./config/passport.js');
-require('./app/routes.js')(app);
+require('./app/routes.js');
 
 app.get('/upload', authAdmin, function(req, res) {
   res.render(path.join(__dirname, "/views/upload.handlebars"),{redirect: false});
