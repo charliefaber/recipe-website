72e394ed58ea4b4ab81e46a97eb96c9c420a8f25
Charlie Faber <charliefaber@gmail.com>
2017-04-26 03:07:09 -0400

    version 1.0

--- app.js
+++ app.js
@@ -1,80 +1,26 @@
-// Server-side Operations
+// ------------- SERVER SIDE -------------
 
-// Reference npm dependencies
+// instantiate variables
 var http = require('http');
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var exphbs = require('express-handlebars');
 var textract = require('textract');
 var path = require('path');
-var fs = require('fs');
-var jsonQuery = require('json-query');
-var methodOverride = require('method-override');
-
-var port = process.env.PORT || 3000;
-var mongoose = require('mongoose');
-//var passport = require('passport');
-// <<<<<<< HEAD
-// //var flash = require('connect-flash');
-// =======
-// var LocalStrategy = require('passport-local').Strategy;
-// var flash = require('connect-flash');
-// >>>>>>> d4328a540bda6202412898395c0c95bd2721956f
-
-// MongoDB dependencies and variables
+// MongoDB setup
 var mongo = require('mongodb');
 var assert = require('assert');
 var MongoClient = mongo.MongoClient;
 var configDb = require('./config/database.js');
-//var auth = require('./config/auth.js');
-var bcrypt = require('bcryptjs');
-var salt = bcrypt.genSaltSync(10);
-
 
 var session = require('express-session');
-// Instantiate express variable
-var app = express();
-// passport.use('local-signup', new LocalStrategy(
-//   {passReqToCallback : true}, //allows us to pass back the request to the callback
-//   function(req, username, password, done) {
-//     funct.localReg(username, password)
-//     .then(function (user) {
-//       if (user) {
-//         console.log("REGISTERED: " + user.username);
-//         req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
-//         done(null, user);
-//       }
-//       if (!user) {
-//         console.log("COULD NOT REGISTER");
-//         req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
-//         done(null, user);
-//       }
-//     })
-//     .fail(function (err){
-//       console.log(err.body);
-//     });
-//   }
-// ));
-
-// Simple route middleware to ensure user is authenticated.
-
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) { return next(); }
-//   req.session.error = 'Please sign in!';
-//   res.redirect('/login');
-// }
-
-
-
-//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var session = require('express-session');
 
-// mongoose.connect(configDb.url);
-//require('./config/passport.js')(passport);
+var bcrypt = require('bcrypt-nodejs');
 
-//app.use(logger('combined'));
+var app = express();
+
 app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -83,86 +29,11 @@ app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard cat',
                   resave: true,
                   saveUninitialized: true,
+                  cookie: {maxAge: 1800000}
                 }));
-
-    MongoClient.connect(configDb.url, function(err, db) {
-    assert.equal(null, err);
-
-        db.collection('documents').find().toArray(function(err, items) {
-          db.collection('documents').remove({path: "C:\\Users\\Jacob\\.IntelliJIdea2016.2\\system\\compiler\\sga-document-archive\\uploads\\.docx"});
-        });
-      });
-var authAdmin = function(req, res, next) {
-  if(req.session && req.session.admin)
-    return next();
-  else
-    res.sendFile(path.join(__dirname + '/views/login.html'))
-};
-
-
-// Generate a salt
-
-// // Hash the password with the salt
-// var hash = bcrypt.hashSync("ChampionOfRrr", salt);
-
-// MongoClient.connect(configDb.url, function(err, db) {
-//   assert.equal(null, err) 
-  
-//   db.collection('users').insert({name: "admin", password: hash});
-
-// });
-/*
-
-
-STORE NEW ADMIN ACCOUNT CODE HERE
-
-
-*/
-
-//app.use(passport.initialize());
-//app.use(passport.session());
-//app.use(flash());
-//app.use(function(req, res, next){
-//  var err = req.session.error,
-//      msg = req.session.notice,
-//      success = req.session.success;
-
-//  delete req.session.error;
- // delete req.session.success;
- // delete req.session.notice;
-
-//   if (err) res.locals.error = err;
-//   if (msg) res.locals.notice = msg;
-//   if (success) res.locals.success = success;
-
-//   next();
-// });
-
-                  // saveUninitialized: true}));
-//app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
-// app.use(function(req, res, next){
-//   var err = req.session.error,
-//       msg = req.session.notice,
-//       success = req.session.success;
-
-//   delete req.session.error;
-//   delete req.session.success;
-//   delete req.session.notice;
-
-//   if (err) res.locals.error = err;
-//   if (msg) res.locals.notice = msg;
-//   if (success) res.locals.success = success;
-
-//   next();
-// });
-
-
 
 app.use(fileUpload());
 app.use(express.static(__dirname));
-
 
 var hbs = exphbs.create({
   helpers: {
@@ -170,29 +41,23 @@ var hbs = exphbs.create({
     inc: function(num) {return num+1;},
   }
 });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-//routers
-//require('./config/passport.js');
 require('./app/routes.js')(app);
 
-app.get('/upload', authAdmin, function(req, res) {
-  res.render(path.join(__dirname, "/views/upload.handlebars"),{redirect: false});
-});
 
-//launch
-/*
-var server = app.listen(port, function(){
-MongoClient.connect(configDB.url, function(err, db) {
-  assert.equal(null, err);
- 
-  db.collection('documents').createIndex({text: "text"});
-  db.close();
-});
-});
-*/
+// // Hash the password with the salt
+// var hash = bcrypt.hashSync("abc123", salt);
 
-app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+// MongoClient.connect(configDb.url, function(err, db) {
+//   assert.equal(null, err) 
+// // //   db.collection('users').insert({name: "admin", password: hash});
+
+// });
+
+
+
+var listener = http.createServer(app).listen(process.env.PORT||3000);
+console.log('Server is listening at port ' + listener.address().port);
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 //app/routes.js
 var assert = require('assert');
-var MongoClient = require('mongodb').MongoClient,
-    test = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 var configDb = require('../config/database.js');
 var path = require('path');
 var express = require('express');
@@ -9,37 +8,158 @@ var app = express();
 var bodyParser = require('body-parser');
 var textract = require('textract');
 //var auth = require('../config/auth.js');
-
-
-var bcrypt = require('bcryptjs');
-
+var bcrypt = require('bcrypt-nodejs');
+var salt = bcrypt.genSaltSync(10);
 
 module.exports = function(app){
 
-//
-//HOME PAGE
-//
-app.get('/', function(req, res) {
+var authAdmin = function(req, res, next) {
+  if(req.session && req.session.admin)
+    return next();
+  else
+  res.sendFile(path.join(__dirname, "../views/login.html"));
+};
 
+app.get('/upload', authAdmin, function(req, res) {
+  res.render(path.join(__dirname, "../views/upload.handlebars"),{redirect: false});
+});
 
+app.get('/', authAdmin, function(req, res) {
     MongoClient.connect(configDb.url, function(err, db) {
     assert.equal(null, err);
 
-        db.collection('documents').find(
-        ).sort({ date: -1}).limit(5).toArray(function(err, items) {
-            if(req.session.admin) {
-                res.render(path.join(__dirname, '../views/indexAdmin.handlebars'), { items: items });
-            }
-            else {
-                res.render(path.join(__dirname, '../views/index.handlebars'), { items: items });
-            }
+        db.collection('recipes').find().sort({ date: -1}).limit(5).toArray(function(err, items) {
+            var admin = req.session.user;
+            console.log(admin == "admin");
+            if(admin == "admin") 
+                res.render(path.join(__dirname, '../views/indexAdmin.handlebars'), { items: items});
+            else 
+                res.render(path.join(__dirname, '../views/index.handlebars'), { items: items});
         });
     });
 });
 
-app.get('/logout', function(req, res) {
-  req.session.destroy();
+app.get('/createAccount', function(req,res) {
+  res.sendFile(path.join(__dirname, "../views/createAccount.html"));
+});
 
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, "../views/login.html"));
+});
+
+app.get('/logout', function(req, res) {
+  var user = req.session.user;
+  console.log("logging out " + user);
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.get('/delete/:recipe',authAdmin,function(req,res) {
+  var recipe = req.params.recipe;
+
+  MongoClient.connect(configDb.url, function(err, db) {
+    assert.equal(null, err);
+
+    db.collection('recipes').find({_id: recipe}).toArray(function(err, items) {
+      db.collection('deleted').insertOne(items[0]);
+      console.log(JSON.stringify(items));
+    });
+
+    db.collection('recipes').remove({_id: recipe});
+    res.redirect('/');
+  });
+});
+
+
+
+
+app.post('/search', function(req, res) {
+  var search = req.body.searchText;         
+  var filter = req.body.filterSelect;     
+  var any = false, breakfast = false, lunch = false, dinner = false, dessert = false, other = false;
+
+  if(filter == null || filter == undefined || filter == "" || filter == "Any Meal") {
+    any = true;
+  }
+  else if(filter == "Breakfast") {
+    breakfast = true;
+  }
+  else if(filter == "Lunch") {
+    lunch = true;
+  }
+  else if(filter == "Dinner") {
+    dinner = true;
+  } 
+  else {
+    other = true;
+  }
+
+  var buttonVals = {filter: filter, any: any, breakfast: breakfast, lunch: lunch, dinner:dinner, other: other};
+
+  MongoClient.connect(configDb.url, function(err, db) {
+    assert.equal(null, err);
+
+    db.collection('recipes').find(
+      {$text: {$search: search}},
+      {score: {$meta: "textScore"}}
+      ).sort({ score: {$meta: "textScore"}}).toArray(function(err, items) {
+        console.log(JSON.stringify(items));
+
+        if(filter == "Breakfast") {
+          items = items.filter(function(a) {
+            return a.meal == "Breakfast";
+          });
+        }
+        else if(filter == "Lunch") {
+          items = items.filter(function(a) {
+            return a.meal == "Lunch";
+          });
+        }
+        else if(filter == "Dinner") {
+          items = items.filter(function(a) {
+            return a.meal == "Dinner";
+          });
+        }
+        else if(filter == "Dessert") {
+          items = items.filter(function(a) {
+            return a.meal == "Dessert";
+          });
+        }
+        else if(filter == "Other") {
+          items = items.filter(function(a) {
+            return a.meal == "Other";
+          });
+        }
+
+        var fail = false;
+        if(items[0] == null) {
+          fail = true;
+        }
+
+        var user = req.session.user;
+        if(user == "admin") 
+          res.render(path.join(__dirname, '../views/resultsAdmin.handlebars'), {search: search, buttonVals: buttonVals, items: items, fail: fail});
+        else 
+          res.render(path.join(__dirname, '../views/results.handlebars'), {search: search, buttonVals: buttonVals, items: items, fail: fail});
+      });
+    db.close();
+  });
+});
+
+app.post('/createAccount', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var hashPass = bcrypt.hashSync(password, salt);
+  MongoClient.connect(configDb.url, function(err, db) {
+    assert.equal(null, err);
+
+      db.collection('users').insert({name: username, password: hashPass});
+      res.sendFile(path.join(__dirname, '../views/login.html'));
+
+    
+    db.close();
+  });
 });
 
 app.post('/checkLogin', function(req, res) {
@@ -50,341 +170,38 @@ app.post('/checkLogin', function(req, res) {
     db.collection('users').find({name: username}).toArray(function(err, items) {
       if(items[0] == undefined) {
         res.redirect('/login');
-          console.log("login failed");
       } 
       else if(bcrypt.compareSync(password, items[0].password)) {
-        console.log("login successful");
         req.session.admin = true;
         req.session.user = username;
-        res.redirect('/upload');
-      
-      }
-    
-    });
-
-  });
-});
-
-// Simple search form query
-app.post('/search', function(req, res) {
-  // Initialize variables
-  var search = req.body.searchText;         //
-  var filter = req.body.filterSelect;       // Selected filter value
-  var relevancy = false, recency = false, highest = false, lowest = false;
-
-  if(filter == null || filter == undefined || filter == "" || filter == "relevancy") {
-    relevancy = true;
-  }
-  else if(filter == "recency") {
-    recency = true;
-  }
-  else if(filter == "highest") {
-    highest = true;
-  }
-  else {
-    lowest = true;
-  }
-
-
-
-  var buttonVals = {filter: filter, relevancy: relevancy, recency: recency, highest: highest, lowest: lowest};
-
-  MongoClient.connect(configDb.url, function(err, db) {
-    assert.equal(null, err);
-
-    db.collection('documents').find(
-      {$text: {$search: search}},
-      {score: {$meta: "textScore"}}
-    ).sort({ score: {$meta: "textScore"}}).toArray(function(err, items) {
-      console.log(JSON.stringify(items));
-
-      if(filter == "recency") {
-        items.sort(function(a, b) {
-          console.log(new Date(a.date));
-          console.log(new Date(b.date));
-
-          return new Date(b.date).getTime() - new Date(a.date).getTime();
-        });
-          //res.render(path.join(__dirname, '/views/resultsTest.handlebars'), {search: search, buttonVals: buttonVals, results: results});
-        console.log("2");
-      }
-      else if(filter == "highest") {
-        items.sort(function(a, b) {
-          return b.amount - a.amount;
-        });
-          //res.render(path.join(__dirname, '/views/resultsTest.handlebars'), {search: search, buttonVals: buttonVals, results: results});
-        console.log("3");
-      }
-      else if(filter == "lowest") {
-        items.sort(function(a, b) {
-          return a.amount - b.amount;
-        });
-        console.log("4");
+        res.redirect('/');
       }
       else {
-        console.log("1")
+        res.redirect('/login');
       }
-      var fail = false;
-      if(items[0] == null) {
-        fail = true;
-      }
-
-      session = req.session.admin;
-
-      if(session == true) 
-        res.render(path.join(__dirname, '../views/resultsAdmin.handlebars'), {search: search, buttonVals: buttonVals, results: items, fail: fail});
-      else {
-      res.render(path.join(__dirname, '../views/results.handlebars'), {search: search, buttonVals: buttonVals, results: items, fail: fail});
-    }
     });
-    //console.log(JSON.stringify(results));
-    db.close();
-  });
-
-});
-
-app.get('/delete/:doc',function(req,res) {
-  var search = req.params.search;
-  var doc = req.params.doc;
-  console.log("delete clicked!");
-
-  MongoClient.connect(configDb.url, function(err, db) {
-    assert.equal(null, err);
-    console.log(doc);
-    db.collection('documents').find({_id: doc}).toArray(function(err, items) {
-      db.collection('deleted').insert(items[0]);
-
-    });
-    db.collection('documents').remove({_id: doc});
-    console.log('deleted!');
-    res.redirect('/');
   });
 });
 
-app.get('/download/:file(*)', function(req, res){
-  var file = req.params.file;
-  console.log(file);
-  var p = path.join(__dirname, "../uploads/", file + ".docx");
-  console.log(p);
-  res.download(p);
-});
-
-//
-//UPLOAD
-//
-
-// app.get('/upload', function(req, res) {
-//   res.render(path.join(__dirname, "../views/upload.handlebars"),{redirect: false});
-
-// });
-
-// POST for upload form submission
 app.post('/upload', function(req, res) {
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  var myFile = req.files.myFile;
+  var name = req.body.name;
+  var _id = name.replace(/\s+/g, '');
 
-  var idText = req.body.idText;
-  idText = idText.trim();
-  var filePath = path.join(__dirname,`../uploads/${idText}.docx`);
+  var meal = req.body.meal;
+  var servings = req.body.servings;
+  var prep = req.body.prep;
+  var date = new Date();
 
+  var upload = {_id: _id, name: name, meal: meal, servings: servings, prep:prep, date: date};
 
-  var doctypeSelect = req.body.doctypeSelect;
-  var dollarText = req.body.dollarText;
-  var dateSelect = req.body.dateSelect;
-  var tagText = req.body.tagText;
-  var bodyText = "";
-  // Use the mv() method to place the file somewhere on your server
-  myFile.mv(filePath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    textract.fromFileWithPath(filePath, function( error, text ) {
-      bodyText = text;
-
-      //var p = path.join(__dirname, "../views/index.handlebars");
-      //res.redirect(p);
-    });
-    // console.log(idText);
-    // console.log(doctypeSelect);
-    // console.log(dollarText);
-    // console.log(dateSelect);
-    // console.log(tagText);
-    //console.log(bodyText);
-    // console.log("");
-    // console.log(req.body);
-    // console.log(req.files);
-  });
-
-  var upload = {idText: idText, doctypeSelect: doctypeSelect, dollarText: dollarText, dateSelect: dateSelect, tagText:tagText, bodyText:bodyText};
+  res.render(path.join(__dirname, '../views/upload.handlebars'), {upload: upload, redirect: true})
   MongoClient.connect(configDb.url, function(err, db) {
     assert.equal(null, err);
-      db.collection('documents').find({_id: idText}).toArray(function(err, item) {
-        if(item[0] == null){
-          db.collection('documents').find().sort({ date: -1}).limit(5).toArray(function(err, items) {
-            res.render(path.join(__dirname, '../views/upload.handlebars'), { redirect: true, upload: upload, success: true });
-          });
-        } else {
-          db.collection('documents').find().sort({ date: -1}).limit(5).toArray(function(err, items) {
-            res.render(path.join(__dirname, '../views/upload.handlebars'), {redirect: true, upload: upload, success: false });
-          });
-        }
-     });
-    db.collection('documents').insertOne( {
-      "_id": idText,
-      "path": filePath,
-      "docType": doctypeSelect,
-      "amount": dollarText,
-      "date": dateSelect,
-      "tagline": tagText,
-      "text": bodyText});
-
+    
+    db.collection('recipes').insertOne(upload);
     db.close();
   });
 
-});
-
-//
-//LOGIN
-//
-
-
-
-app.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/login.html"));
-});
-
-
-// app.get('/logout', function(req,res){
-//     var name = req.user.username;
-//     console.log("LOGGING OUT " + name)
-//     req.logout();
-//     res.redirect('/');
-//     req.session.notice = name + " has successfully logged out.";
-// });
-
-//
-//ADVANCED SEARCH PAGE
-//
-app.get('/advanced', function(req, res) {
-  res.sendFile(path.join(__dirname, "/../views/advanced.html"));
-});
-
-app.get('/help', function(req, res) {
-   res.sendFile(path.join(__dirname, "/../views/help.html"));
-
-});
-//
-//ADVANCED SEARCH CODE
-//
-
-// Handles submission of advanced search form
-app.post('/advancedSearch', function(req, res) {
-
-// Initialize variables with values from form fields
-  var search = req.body.advText;                        // Search String
-  var filter = req.body.advFilterSelect;                // Selected value of filter dropdown
-  var relevancy = false, recency = false, highest = false, lowest = false;
-  // Store the value of each filter option in respective boolean variables
-  if(filter == null || filter == undefined || filter == "" || filter == "relevancy")
-     relevancy = true;
-  else if(filter == "recency")
-    recency = true;
-  else if(filter == "highest")
-    highest = true;
-  else
-    lowest = true;
-
-  // Document Type Checkboxes
-  var billCheck = "";               // Bill
-  var resCheck = "";                // Resolution
-  // If value of Checkbox is 'on', box is checked, therefore store "checked" in respective variable
-  if(req.body.billCheck == 'on') {billCheck = "checked";}
-  if(req.body.resCheck == 'on') {resCheck = "checked";}
-  console.log(billCheck + " " + resCheck);
-  // Year Range
-  var yearMin = req.body.yearMin;
-  var yearMax = req.body.yearMax;
-  // If min or max not provided, assign default year range of 0 to 3000
-  if(yearMin == null || yearMin == undefined || yearMin == "")
-    yearMin = 0;
-  if(yearMax == null || yearMax == undefined || yearMax == "")
-    yearMax = 3000;
-  console.log(yearMin + " " + yearMax);
-  // Amount Range
-  var amtMin = req.body.amtMin;
-  var amtMax = req.body.amtMax;
-  // If min or max not provided, assign default amount range of 0 to 100000
-  if(amtMin == null || amtMin == undefined || amtMin == "")
-    amtMin = 0;
-  if(amtMax == null || amtMax == undefined || amtMax == "")
-    amtMax = 100000;
-
-  console.log(amtMin + " " + amtMax);
-
-  // Store values of buttons in JavaScript object
-  var buttonVals = {filter: filter, relevancy: relevancy, recency: recency, highest: highest, lowest: lowest};
-
-// Connect to Mongo database
-  MongoClient.connect(configDb.url, function(err, db) {
-    assert.equal(null, err);        // Check for errors
-
-    // Query documents collection
-    db.collection('documents').find(
-      {$text: {$search: search}},             // User's text passed in search variable
-      {score: {$meta: "textScore"}}           // Sort documents by score of match
-    ).sort({ score: {$meta: "textScore"}}).toArray(function(err, items) { // Convert to array
-      console.log(JSON.stringify(items));
-      // Filter the array based on the advanced form fields
-      items = items.filter(function(item) {
-        // Get year of current document being filtered
-        var year = new Date(item.date).getFullYear();
-
-        // If both bill and resolution are checked
-        if(billCheck == "checked" && resCheck == "checked")
-          return(year >= yearMin && year <= yearMax && item.amount >= amtMin && item.amount <= amtMax)
-        // Else if only bill is checked
-        else if(billCheck == "checked")
-          return(item.docType == "BILL" && year >= yearMin && year <= yearMax && item.amount >= amtMin && item.amount <= amtMax);
-        // Else if only resolution is checked
-        else if(resCheck == "checked") {
-          return(item.docType == "RESOLUTION" && year >= yearMin && year <= yearMax && item.amount >= amtMin && item.amount <= amtMax);
-        }
-      });
-
-      console.log(JSON.stringify(items));
-      // Use value of filter to determine how to sort results
-      if(filter == "recency") {
-        // Sort based on recency of date
-        items.sort(function(a, b) {
-          return new Date(b.date).getTime() - new Date(a.date).getTime();
-        });
-      }
-      else if(filter == "highest") {
-        // Sort based on amount of document, highest first
-        items.sort(function(a, b) {
-          return b.amount - a.amount;
-        });
-      }
-      else if(filter == "lowest") {
-        // Sort based on amount of document, lowest first
-        items.sort(function(a, b) {
-          return a.amount - b.amount;
-        });
-      }
-      var fail = false;
-      if(items[0] == null) {
-
-        fail = true;
-        
-
-      }
-      // Render results handlebars template, passing variables containing search, button values, and sorted results
-      res.render(path.join(__dirname, '../views/results.handlebars'), {search: search, buttonVals: buttonVals, results: items, fail: fail});
-    });
-    db.close();
-  });
 });
 };
